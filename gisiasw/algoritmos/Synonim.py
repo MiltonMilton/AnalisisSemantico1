68bb5a39e3ec8f5ddaf5e06856ada25ed034f6e4
Milton <anderstuhl@gmail.com>
2018-05-31 16:39:28 -0300

    sinonimos sin repetir

--- gisiasw/algoritmos/Synonim.py
+++ gisiasw/algoritmos/Synonim.py
@@ -1,4 +1,5 @@
 from nltk.corpus import wordnet as wn
+import sets
 
 def getSinonimo(word):
     sinonimos = []
@@ -7,8 +8,8 @@ def getSinonimo(word):
             sinonimos.append({
                 "word": l.name(),
                 "synset": l.synset().name(),
-                #"hypernimos": getHipernimo(j.hypernyms()),
-                #"hiponimos": [l.lemma_names() for l in j.hyponyms()],
+                # "hypernimos": getHipernimo(j.hypernyms()),
+                # "hiponimos": [l.lemma_names() for l in j.hyponyms()],
                 "tipo": l.synset().lexname().split('.')[0],
                 "distance": compareWords(word, l.name()),
                 "definition": l.synset().definition(),
@@ -18,12 +19,12 @@ def getSinonimo(word):
     return sinonimos
 
 def getListaSinonimos(word):
-    list = []
+    lista = []
     syns = wn.synsets(word)
     for ss in range(len(syns)):
         for l in range(len(syns[ss].lemmas())):
-            list.append(syns[ss].lemmas()[l].name())
-    return list
+            lista.append(syns[ss].lemmas()[l].name())
+    return list(sets.Set(lista))
 
 def getHipernimo(hypernimos):
     return [l.lemma_names()for l in hypernimos],
@@ -31,4 +32,4 @@ def getHipernimo(hypernimos):
 def compareWords(w1, w2):
     w1 = wn.synsets(w1)[0]
     w2 = wn.synsets(w2)[0]
-    return  w1.wup_similarity(w2)
+    return  w1.wup_similarity(w2)    
