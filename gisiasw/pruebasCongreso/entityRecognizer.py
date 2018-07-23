e8883f8f549901f2859d8c617e2901bba7c30aab
Milton <anderstuhl@gmail.com>
2018-07-22 23:28:32 -0300

    entity recognizer refinado

--- gisiasw/pruebasCongreso/entityRecognizer.py
+++ gisiasw/pruebasCongreso/entityRecognizer.py
@@ -6,7 +6,13 @@ class EntityRecognizer:
         textrazor.api_key = "8cdcb70d8ae86e21ac318e3d8cd6fb2b456f6e9f984d39267fa78d32"
         client = textrazor.TextRazor(extractors=["entities", "topics"])
         response = client.analyze_url(url)
-        entities = []
+        entities75 = []
+        entities50 = []
+        entities20 = []
         for entity in response.entities():
-            if entity.relevance_score > 0.75: entities.append(entity.id)
-        return list(set(entities))[:5] #elimina duplicados y toma los primeros 5
+            if entity.relevance_score > 0.75: entities75.append(entity.id)
+            if entity.relevance_score > 0.50: entities50.append(entity.id)
+            if entity.relevance_score > 0.20: entities20.append(entity.id)
+        if len(list(set(entities75))) > 4: return list(set(entities75))[:5]       
+        elif len(list(set(entities50))) > 4: return list(set(entities50))[:5]
+        else: return list(set(entities20))[:5]    
