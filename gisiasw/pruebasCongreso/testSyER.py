a9a032207b9890ae815fd8709f99410af1eb2d2f
Milton <anderstuhl@gmail.com>
2018-07-27 22:13:19 -0300

    prueba definitiva 1

--- gisiasw/pruebasCongreso/testSyER.py
+++ gisiasw/pruebasCongreso/testSyER.py
@@ -1,11 +1,13 @@
 from searcher import Searcher
 from entityRecognizer import EntityRecognizer
+from similarityMeasurer import SimilarityMeasurer
 
 s = Searcher()
 er = EntityRecognizer()
+sm = SimilarityMeasurer()
 
 keys = ["machine","learning","python"]
-n = 10
+n = 20
 
 rg = s.search(keys,"google",n) #resultados google
 rb = s.search(keys,"bing",n) #resultados bing
@@ -14,9 +16,17 @@ rb = s.search(keys,"bing",n) #resultados bing
 print "----------google-----------"
 for r in rg:
     print "url: " + str(r)
-    print "entities: " + str(er.recognizeAndCheckSynset(r))
+    entities = er.recognizeAndCheckSynset(r)
+    print "entities: " + str(entities)
+    for clave in keys:
+        for entity in entities:
+            print "clave: " + clave + " ;entidad: " + entity + " ;medidas: " + str(sm.measure(clave,entity))
 
 print "----------bing-----------"
 for r in rb:
     print "url: " + str(r)
-    print "entities: " + str(er.recognizeAndCheckSynset(r))
+    entities = er.recognizeAndCheckSynset(r)
+    print "entities: " + str(entities)
+    for clave in keys:
+        for entity in entities:
+            print "clave: " + clave + " ;entidad: " + entity + " ;medidas: " + str(sm.measure(clave,entity))
